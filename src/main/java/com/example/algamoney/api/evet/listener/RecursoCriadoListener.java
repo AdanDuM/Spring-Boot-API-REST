840b9d4997dde99f05c672249fdb368d14aaef10
AdanDuM <adandum@hotmail.com>
2018-07-29 17:05:38 -0300

    Commit inicial

---
+++ src/main/java/com/example/algamoney/api/evet/listener/RecursoCriadoListener.java
@@ -0,0 +1,30 @@
+package com.example.algamoney.api.evet.listener;
+
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.algamoney.api.evet.RecursoCriadoEvento;
+
+@Component
+public class RecursoCriadoListener implements ApplicationListener<RecursoCriadoEvento> {
+
+	@Override
+	public void onApplicationEvent(RecursoCriadoEvento recursoCriadoEvento) {
+		HttpServletResponse response = recursoCriadoEvento.getResponse();
+		Long codigo = recursoCriadoEvento.getCodigo();
+		
+		adicionarHeaderLocation(response, codigo);
+	}
+
+	private void adicionarHeaderLocation(HttpServletResponse response, Long codigo) {
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(codigo).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+	}
+
+}
---
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -0,0 +1,81 @@
+package com.example.algamoney.api.resource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.algamoney.api.evet.RecursoCriadoEvento;
+import com.example.algamoney.api.exception.AlgamoneyExceptionHandler.Erro;
+import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.repository.LancamentoRepository;
+import com.example.algamoney.api.repository.filter.LancamentoFilter;
+import com.example.algamoney.api.service.LancamentoService;
+import com.example.algamoney.api.service.exception.PessoaInexistenteOuInativaException;
+
+@RestController
+@RequestMapping("/lancamentos")
+public class LancamentoResource {
+
+	@Autowired
+	private LancamentoRepository lancamentoRepository;
+
+	@Autowired
+	private LancamentoService lancamentoService;
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@GetMapping
+	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
+		return lancamentoRepository.filtrar(lancamentoFilter);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
+		
+		publisher.publishEvent(new RecursoCriadoEvento(this, response, lancamentoSalvo.getCodigo()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<?> buscarPeloCodigo(@PathVariable Long codigo) {
+		Lancamento lancamentoEncontrado = lancamentoRepository.findOne(codigo);
+		
+		if (lancamentoEncontrado != null) {
+			return ResponseEntity.ok(lancamentoEncontrado);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@ExceptionHandler({ PessoaInexistenteOuInativaException.class })
+	public ResponseEntity<Object> handlePessoaInexistenteOuInativaException(PessoaInexistenteOuInativaException ex) {
+		String mensagemUsuario = messageSource.getMessage("pessoa.inexistente-ou-inativa", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.toString();
+		
+		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+		return ResponseEntity.badRequest().body(erros);
+	}
+
+}
